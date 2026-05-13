b387f55c1b528e3db0d6e728a873d5539301c65b
xianrentesuchi-bit <xianrentesuchi@gmail.com>
2026-05-13 16:50:34 +0900

    Update comments.js

--- api/youtube/comments.js
+++ api/youtube/comments.js
@@ -1,6 +1,23 @@
 const { getYouTube } = require('../youtube');
+const axios = require('axios');
+const { INSTANCE } = require('../invidious');
 
 async function getComments(videoId) {
+    for (let url of INSTANCE) {
+        try {
+            const baseUrl = url.endsWith('/') ? url.slice(0, -1) : url;
+            const res = await axios.get(`${baseUrl}/comments/${videoId}`, { timeout: 3000 });
+            if (res.data && res.data.comments) {
+                return res.data.comments.map(c => ({
+                    author: c.author,
+                    text: c.content,
+                    authorThumb: c.authorThumbnails[0].url
+                }));
+            }
+        } catch (e) {
+            continue;
+        }
+    }
     const yt = await getYouTube();
     try {
         const comments = await yt.getComments(videoId);
