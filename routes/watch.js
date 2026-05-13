7b2792bd31d1dd28fa9bdd2aa268b8e9df80f934
xianrentesuchi-bit <xianrentesuchi@gmail.com>
2026-05-14 06:32:26 +0900

    Update watch.js

--- routes/watch.js
+++ routes/watch.js
@@ -3,13 +3,18 @@ const router = express.Router();
 const getStream = require('../api/youtube/getstream');
 const getRelated = require('../api/youtube/kanrendouga');
 const getComments = require('../api/youtube/comments');
+const getVideoInfo = require('../api/youtube/videoinfo');
+const getEduUrl = require('../api/youtube/edu');
 
 router.get('/', async (req, res) => {
     const videoId = req.query.v;
     const streamUrl = await getStream(videoId);
     const related = await getRelated(videoId);
     const comments = await getComments(videoId);
-    res.render('watch', { videoId, streamUrl, related, comments });
+    const videoInfo = await getVideoInfo(videoId);
+    res.render('watch', { videoId, streamUrl, related, comments, videoInfo });
 });
+
+router.get('/api/youtube/edu', getEduUrl);
 
 module.exports = router;
