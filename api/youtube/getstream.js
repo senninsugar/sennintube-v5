472c7ab1bcd95448d3c0e3bae1fffadaa2a6aa4c
xianrentesuchi-bit <xianrentesuchi@gmail.com>
2026-05-13 16:49:01 +0900

    Update getstream.js

--- api/youtube/getstream.js
+++ api/youtube/getstream.js
@@ -1,6 +1,19 @@
 const { getYouTube } = require('../youtube');
+const axios = require('axios');
+const { INSTANCE } = require('../invidious');
 
 async function getStream(videoId) {
+    for (let url of INSTANCE) {
+        try {
+            const baseUrl = url.endsWith('/') ? url.slice(0, -1) : url;
+            const res = await axios.get(`${baseUrl}/videos/${videoId}`, { timeout: 3000 });
+            if (res.data && res.data.formatStreams) {
+                return res.data.formatStreams[0].url;
+            }
+        } catch (e) {
+            continue;
+        }
+    }
     const yt = await getYouTube();
     const info = await yt.getBasicInfo(videoId);
     return info.streaming_data?.formats[0]?.url || info.streaming_data?.adaptive_formats[0]?.url;
