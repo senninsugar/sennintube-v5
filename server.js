24aef8e563dc2e11e12c668e4e60e0fe0c91fb6e
xianrentesuchi-bit <xianrentesuchi@gmail.com>
2026-05-14 20:03:45 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-​app.set('view engine', 'ejs');
+
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
-​const homeRouter = require('./routes/home');
+
+const homeRouter = require('./routes/home');
 const searchRouter = require('./routes/search');
 const watchRouter = require('./routes/watch');
 const suggestRouter = require('./routes/suggests');
 const channelRouter = require('./routes/channel');
 const getEduUrl = require('./api/youtube/edu');
-​app.use('/', homeRouter);
+
+app.use('/', homeRouter);
 app.use('/search', searchRouter);
 app.use('/watch', watchRouter);
 app.use('/api/suggests', suggestRouter);
 app.use('/channel', channelRouter);
 app.get('/api/youtube/edu', getEduUrl);
-​const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(Server running on port ${PORT}));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
